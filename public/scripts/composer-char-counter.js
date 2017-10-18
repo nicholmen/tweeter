4c3fa21521ddcd06e6b319b198c900ec98757bf6
Thomas Nicholson <nicholmen@gmail.com>
2017-10-18 01:02:07 +0000

    made jquery to support character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,12 @@
-console.log('testing character counter file loading')
+$(document).ready(function() {
+  $('.new-tweet textarea').on('keyup', function() {
+    var availableCharacters = 140 - $(this).val().length;
+    var counterCount = $(this).parent().find('.counter').html(availableCharacters);
+    // availableCharacters < 0 ? counterCount.css('color', 'red') : counterCount.css('color', '')
+    if (availableCharacters < 0) {
+      counterCount.css('color', 'red')
+    } else {
+      counterCount.css('color', '')
+    }
+  })
+});
