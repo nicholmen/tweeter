c85ed2be90f78fc91a87646ab69182771685af88
Thomas Nicholson <nicholmen@gmail.com>
2017-10-19 03:26:07 +0000

    completed form validation..... providing error alerts for users if they try to submit a tweet that is longer than 140 characters or if they click submit with an empty form. also adjusted to reset counter to 140 after succesful submission of tweet. also added css to .tweeet-container p to prevent issues if adding a tweet is all one word and wider than the width of the container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,5 @@
 $(function () {
 
-  // Fake data taken from tweets.json
-  // var data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "<script>alert('uh oh!');</script> Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-
   //take in an array of tweet objects and then appending each one to the #tweets-container
   function renderTweets(tweets) {
     // loops through tweets
@@ -107,15 +59,26 @@ $(function () {
       } 
     })
   }
-
   loadTweets();
 
 
-  // prevent the existing form submission to keep the browser from leaving the page
+  // prevent the existing form submission to keep the browser from leaving the page, use ajax to make the request
+  const $tweetInput = $("#tweetInput");
+  const $tweetAlert = $('#tweetAlert');
+
   $('#new-tweet-form').on('submit', function (event) {
     event.preventDefault();
     var form = this;
     var input = $(this).serialize();
+    console.log('input: ', input)
+    console.log('tweet-val', $tweetInput.val())
+    
+    if (!$tweetInput.val()) {
+      return alert("add something");
+    }
+    else if (($tweetInput.val()).length > 140) {
+      return alert("less characters please");
+    }
     // instead, submit the form data using Ajax
     $.ajax({
       method: 'post',
@@ -125,6 +88,7 @@ $(function () {
       // empty the text from the form field upon post
       form.reset();
       loadTweets();
+      $('.counter').html('140');
     })
   })
 })
